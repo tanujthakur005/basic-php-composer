1925c03eb3a1a71ab5421441661148cd5689228e
Tanuj Thakur <134038039+tanujthakur005@users.noreply.github.com>
2024-03-05 18:52:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,7 @@
  <body>
  <?php echo '<p>Hello World this is the testing heloooooooooooooooooooo tttttttttttttttttttttttttttt
 
-This is the testing /n
-
-HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH /n
-HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+New page
 
 </p>'; ?> 
  </body>
