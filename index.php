fb82b17f2b5008a989f874152771bff893fcd66b
Tanuj Thakur <134038039+tanujthakur005@users.noreply.github.com>
2024-03-05 16:27:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
-<?php
-
-require __DIR__ . '/vendor/autoload.php';
-use Carbon\Carbon;
-
-
-printf("Right now in New York is %s.\n", Carbon::now('US/Eastern')->format('Y-m-d g:i:s A'));
-
-printf("Tomorrow in New York will be %s.\n", Carbon::tomorrow('US/Eastern')->format('Y-m-d'));
-
-printf("Right now in Paris is %s\n", Carbon::now('Europe/Paris'));
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
+</html>
