0cd3289efc040fc156d6437e2420968485ce5f3c
Tanuj Thakur <134038039+tanujthakur005@users.noreply.github.com>
2024-03-05 16:46:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World this is the testing heloooooooooooooooooooo</p>'; ?> 
+ <?php echo '<p>Hello World this is the testing heloooooooooooooooooooo tttttttttttttttttttttttttttt</p>'; ?> 
  </body>
 </html>
